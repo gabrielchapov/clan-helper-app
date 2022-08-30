90fbff32f935e0b5e73b067369369b9b24564c85
gabrielchpv <uliseschapov97@gmail.com>
2022-08-30 11:37:39 -0300

    feat: Initialized project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+// import {BrowserRouter, Routes, Route} from 'react-router-dom'
+function App() {
+
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/WeeklyRotation.tsx
@@ -0,0 +1,5 @@
+export const WeeklyRotation = () => {
+  return (
+    <div>Weekly Rotation</div>
+  )
+}
